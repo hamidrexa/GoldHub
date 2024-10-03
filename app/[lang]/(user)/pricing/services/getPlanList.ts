6ac3f3c0acec9b6d01c0928763feb2ffd1a3a123
ehsaneona <ehsaneona@gmail.com>
2024-10-03 10:41:53 +0330

    WIP

--- app/[lang]/(user)/pricing/services/getPlanList.ts
+++
@@ -1,9 +0,0 @@
-import { fetcher } from '@/libs/utils';
-import { Locale } from '@/i18n-config';
-
-export function getPlanList(lang?: Locale) {
-    return fetcher({
-        url: '/v1/transactions/plans',
-        lang,
-    });
-}
--- app/[lang]/(user)/pricing/services/payment.ts
+++
@@ -1,9 +0,0 @@
-import { fetcher } from '@/libs/utils';
-
-export function payment(body) {
-    return fetcher({
-        url: '/v1/transactions',
-        method: 'POST',
-        body,
-    });
-}
